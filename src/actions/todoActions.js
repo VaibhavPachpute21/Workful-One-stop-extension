9ddc07ade16dd7185c242d45ef17254c2f9d3065
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-02-27 15:58:24 +0530

    local storage

--- src/actions/todoActions.js
+++ src/actions/todoActions.js
@@ -1,9 +1,14 @@
-export const addTodo=(todo)=>(dispacth)=>{
+export const addTodo=(todo)=>(dispacth,getState)=>{
     dispacth({type:"ADD_TODO",payload:todo})
+    localStorage.setItem(
+        "todos",JSON.stringify(getState().todoReducer.todos))
 }
 
-export const removeTodo=(todo)=>(dispacth)=>{
+export const removeTodo=(todo)=>(dispacth,getState)=>{
     dispacth({type:"REMOVE_TODO",payload:todo })
+    const todos = getState().todoReducer.todos;
+    localStorage.setItem("todos", JSON.stringify(todos))
+
 
     
 }
--- src/store.js
+++ src/store.js
@@ -2,13 +2,23 @@ import {createStore, combineReducers, applyMiddleware } from 'redux'
 import {todoReducer} from './reducers/todoReducers'
 import thunk from 'redux-thunk'
 
+const todos = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+
+
 const rootReducer=combineReducers({
     todoReducer:todoReducer
 })
 
+const initialState={
+    todoReducer:{
+        todos:todos
+
+    }
+}
 
 const store=createStore(
     rootReducer,
+    initialState,
     applyMiddleware(thunk)
 );
 
