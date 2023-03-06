60407526d7dff4b1e2f15bcf1124a12197e1fc01
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-03-06 18:24:01 +0530

    little more styles

--- src/Components/Weather/Weather.js
+++ src/Components/Weather/Weather.js
@@ -92,31 +92,47 @@ const Weather = () => {
 
   return (
     <div
-      style={weather.weather == 'Clouds' ? {
+      style={
+        // weather.weather == 'Clouds' ? 
+      {
         border: '2px solid black', display: 'flex', flexDirection: 'column', justifyContent: 'center',
-        width: '168px', height: 'max-content', padding: '10px', backgroundColor: '#9DD5FF',
+        width: '168px', height: 'max-content', padding: '10px', //backgroundColor: '#9DD5FF'
+        backgroundImage:'linear-gradient(to bottom,#9DD5FF,#118AE3)',
         borderRadius: '10px', fontSize: '18px', color: 'white'
-      } : weather.weather == 'Clear' ? {
-        border: '2px solid black', display: 'flex', flexDirection: 'column', justifyContent: 'center',
-        width: '168px', height: 'max-content', padding: '10px', backgroundColor: '#9DD5FF', //backgroundImage: 'linear-gradient(to right, blue , red)',
-        borderRadius: '10px', fontSize: '18px', color: 'white'
-      } : { backgroundColor: 'black' }}>
+      } 
+      // : weather.weather == 'Clear' ? {
+      //   border: '2px solid black', display: 'flex', flexDirection: 'column', justifyContent: 'center',
+      //   width: '168px', height: 'max-content', padding: '10px', backgroundColor: '#9DD5FF', //backgroundImage: 'linear-gradient(to right, blue , red)',
+      //   borderRadius: '10px', fontSize: '18px', color: 'white'
+      // } : { backgroundColor: 'black' }
+      }>
       <form onSubmit={search}>
         <input
-        style={{backgroundColor:'#F7F8F9', border:'1px solid #DBE2E9 !important', borderRadius:'100px',padding:'5px',fontSize:'12px'}}
+        style={{backgroundColor:'#F7F8F9', border:'1px solid #DBE2E9 !important', borderRadius:'100px',padding:'5px',fontSize:'12px',zIndex:'999'}}
          placeholder='Search for City' onChange={(e) => { setCity(e.target.value) }} type={'text'} />
         {/* <button type='submit' onClick={search}>Search</button> */}
       </form>
 
-      <div style={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around', }}>
-        <div>{Math.trunc(weather.temp)}&#8451;</div>
-        <div><img src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="" /></div>
+      <div style={{ display: 'flex', flexDirection: 'row' }}>
+        <div
+        style={{fontSize:'26px',fontWeight:'500'}}
+        >{Math.trunc(weather.temp)}&#8451;</div>
+        <div ><img style={{position:'absolute',paddingLeft:'20px',marginTop:'-20px',zIndex:'1'}} src={`http://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="" /></div>
       </div>
 
-      <div>{weather.weather}</div>
-      <div>{weather.weatherDes}</div>
-      <div>Feels like {Math.trunc(weather.feels)}&#8451;</div>
-      <div>{weather.name + ' ' + weather.country}</div>
+      <div
+      style={{fontSize:'22px',fontWeight:'500'}}      
+      >{weather.weather}</div>
+      <div
+      style={{fontSize:'16px',fontWeight:'400',textTransform:'capitalize'}}      
+      >{weather.weatherDes}</div>
+
+      <div
+      style={{fontSize:'16px',fontWeight:'400',textTransform:'capitalize'}}       
+      >Feels like <strong>{Math.trunc(weather.feels)}&#8451;</strong></div>
+      <div
+      style={{fontSize:'20px',fontWeight:'400'}}      
+      >{weather.name + ' ' + weather.country}</div>
 
 
 
