1a91cce323e5e68597ab6f556feff6317c22f6f9
VaibhavPachpute21 <vpachpute21@gmail.com>
2023-09-10 14:10:29 +0530

    hourly notification

--- public/background.js
+++ public/background.js
@@ -1,11 +1,27 @@
+function createAlarm() {
+    chrome.alarms.create(
+        "scheduledNotification",
+        {
+            delayInMinutes: 1,
+            periodInMinutes: 60
+        }
+    );
+}
+
+function clearAlarm() {
+    chrome.alarms.clear("scheduledNotification", () => {
+      console.log("Alarm cleared");
+    });
+  }
+
 chrome.alarms.onAlarm.addListener(
     () => {
         chrome.notifications.create(
             {
                 type: "basic",
                 iconUrl: "icon.png",
-                title: "Test",
-                message: "Testing!",
+                title: "Important",
+                message: "It's time to take a break from work. Stretch, relax, and recharge for a few minutes. Your well-being matters!",
                 silent: false
             },
             () => { }
@@ -15,21 +31,13 @@ chrome.alarms.onAlarm.addListener(
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
         console.log(request);
-        if (request.time)
+        if (request.time){
             createAlarm();
+        }else if (request.clearAlarm) {
+            clearAlarm();
+          }
 
-        sendResponse(() => {
-            return false
-        });
+        sendResponse({success: true});
     }
 );
 
-function createAlarm() {
-    chrome.alarms.create(
-        "scheduledNotification",
-        {
-            delayInMinutes: 1,
-            periodInMinutes: 1
-        }
-    );
-}
--- src/Components/Greet/Greet.js
+++ src/Components/Greet/Greet.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import {BsAlarm} from 'react-icons/bs'
-
+import { BsAlarm } from 'react-icons/bs'
+import { BiAlarmOff } from 'react-icons/bi'
 const Greet = () => {
     const [greetMsg, setgreetMSG] = useState('')
     const [quote, setQuote] = useState('')
     let t = new Date().getHours()
+    let [alarmOn, setAlarmOn] = useState(true);
 
 
     useEffect(() => {
@@ -20,21 +21,44 @@ const Greet = () => {
         getQuotes();
     }, [t])
 
+    useEffect(() => {
+        const storedAlarmOn = localStorage.getItem('alarmOn');
+        if (storedAlarmOn !== null) {
+            setAlarmOn(storedAlarmOn === 'true'); // Convert the string to a boolean
+        }
+    }, []);
+
 
     async function getQuotes() {
         try {
-          const response = await fetch('https://type.fit/api/quotes');
-          if (!response.ok) {
-            throw new Error('Network response was not ok');
-          }
-          const data = await response.json();
-          const randomIndex = Math.floor(Math.random() * data.length);
-          const randomQuote = data[randomIndex].text;
-          setQuote(randomQuote);
+            const response = await fetch('https://type.fit/api/quotes');
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            const data = await response.json();
+            const randomIndex = Math.floor(Math.random() * data.length);
+            const randomQuote = data[randomIndex].text;
+            setQuote(randomQuote);
         } catch (error) {
-          console.error('Error fetching quotes:', error);
+            console.error('Error fetching quotes:', error);
         }
-      }
+    }
+
+    const toggleAlarm = () => {
+        if (alarmOn) {
+            chrome.runtime.sendMessage({ clearAlarm: true }, function (response) {
+                console.log(response);
+            });
+            setAlarmOn(false);
+            localStorage.setItem('alarmOn', 'false');
+        } else {
+            chrome.runtime.sendMessage({ time: "1" }, function (response) {
+                console.log(response);
+            });
+            setAlarmOn(true);
+            localStorage.setItem('alarmOn', 'true');
+        }
+    };
 
 
 
@@ -43,7 +67,7 @@ const Greet = () => {
         <div style={{
             display: 'flex', width: '400px', flexDirection: 'column', backgroundColor: '#4169e1',
             fontSize: '20px', padding: '10px', color: 'white', borderRadius: '10px',
-            justifyContent: 'center', border:'1px solid white', fontFamily:'serif',
+            justifyContent: 'center', border: '1px solid white', fontFamily: 'serif',
         }}>
             <div
                 style={{ fontSize: '18px', fontWeight: 'bold', color: 'white', }}
@@ -51,16 +75,11 @@ const Greet = () => {
             <div
                 style={{ fontSize: '16px', fontWeight: '500', color: 'white' }}
             >{quote.toString()}</div>
-            <button id='notifyButton'
-            onClick={() => {
-                chrome.runtime.sendMessage({ time: "1" }, function (response) {
-                    console.log(response);
-                });
-            }}
-             style={{
-                position:'absolute',float:'right',right:'20px',cursor:'pointer'
-            }}>
-                <BsAlarm/>
+            <button onClick={() => { toggleAlarm() }}
+                style={{
+                    position: 'absolute', float: 'right', right: '20px', cursor: 'pointer'
+                }}>
+                {alarmOn ? <BiAlarmOff /> : <BsAlarm />}
             </button>
 
         </div>
